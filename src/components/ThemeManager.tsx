d7e8c233c99fa607451e0653f0c4e751cdf416ab
Ahmd Abdo <rehlathadaf@gmail.com>
2025-05-04 10:07:30 +0300

    Create ThemeManager.tsx

---
+++ src/components/ThemeManager.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ThemeManager() {
+  return (
+    <div className="text-center text-gray-400">
+      إعدادات تخصيص المظهر ستتوفر قريبًا.
+    </div>
+  );
+}
